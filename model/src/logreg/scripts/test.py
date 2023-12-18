e90356a1ee2522437ee8458bfe89d55a498f62d7
Zhirong Lin <zhirong.lin@data61.csiro.au>
2023-12-18 15:58:51 +1000

    Generate dockerfile and remove entrypoint

--- model/src/logreg/scripts/test.py
+++ model/src/logreg/scripts/test.py
@@ -1,10 +1,10 @@
 import mlflow
 
 def main():
-    # Pull in the egonn model
     pyfunc_model = mlflow.pyfunc.load_model(model_uri=f"/model")
     sig = pyfunc_model._model_meta._signature.to_dict()
     print(sig)
+    print(pyfunc_model._model_meta)
 
 
 if __name__ == "__main__":
