b1c3c4113610d408449455eadfce53cc4e1b90ad
Zhirong Lin <zhirong.lin@data61.csiro.au>
2023-12-20 16:46:23 +1000

    Change build context to base directory

--- generic/build.bash
+++ generic/build.bash
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-SCRIPTPATH="$(dirname "$0")"
 MODEL_URI="models:/tracking-quickstart/1"
 DOCKER_IMAGE="testi"
+
+SCRIPTPATH="$(dirname "$0")"
+DOCFILE=$SCRIPTPATH/dockerfile/Dockerfile
 
 export MLFLOW_TRACKING_URI="http://ras-b2-ph.nexus.csiro.au:5000"
 
 mlflow models generate-dockerfile --model-uri "$MODEL_URI" --output-directory "$SCRIPTPATH/dockerfile"
 
 # Add `as base` to the initial build stage
-sed -i '/FROM ubuntu/ s/$/ as base/' $SCRIPTPATH/dockerfile/Dockerfile
+sed -i '/FROM ubuntu/ s/$/ as base/' $DOCFILE
 
 # Remove the entrypoint command in the dockerfile as we will define a new one later
-sed -i '/ENTRYPOINT/d' $SCRIPTPATH/dockerfile/Dockerfile
+sed -i '/ENTRYPOINT/d' $DOCFILE
 
-cat $SCRIPTPATH/Install.Dockerfile >> $SCRIPTPATH/dockerfile/Dockerfile
+# Edit the COPY command to work with our build context
+sed -i '/model_dir/c\COPY dockerfile/model_dir /opt/ml/model' $DOCFILE
 
-cp -r ./src ./dockerfile
+cat $SCRIPTPATH/Install.Dockerfile >> $DOCFILE
 
-docker build --tag "$DOCKER_IMAGE" $SCRIPTPATH/dockerfile
+docker build --tag "$DOCKER_IMAGE" --file $DOCFILE $SCRIPTPATH
