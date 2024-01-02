fcde42506ac712064678d491d2648fc332894ff0
Zhirong Lin <zhirong.lin@data61.csiro.au>
2024-01-02 15:08:33 +1000

    Lint and add linters to dev requirements

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import os
-from os.path import join
-import mlflow
-import jinja2
-from configobj import ConfigObj
 import json
-from dataclasses import dataclass, asdict
+import os
+from dataclasses import asdict, dataclass
+from os.path import join
 from pathlib import Path
+
+import jinja2
+import mlflow
+from configobj import ConfigObj
 
 LOCAL_URI = "dockerfile/model_dir"
 CONFIG_NAME = "config.cfg"
@@ -13,116 +14,116 @@ CONFIG_NAME = "config.cfg"
 # TODO: Finish filling out with:
 # - https://numpy.org/devdocs/user/basics.types.html
 # - http://wiki.ros.org/msg
-TENSOR_TO_ROS = {
-  "float64": "float64",
-  "int64": "int64"
-}
+TENSOR_TO_ROS = {"float64": "float64", "int64": "int64"}
 
 # absolute path to the location of this script
-__location__ = os.path.realpath(
-  join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(join(os.getcwd(), os.path.dirname(__file__)))
 
 
 @dataclass
-class Msg():
-  type: str
-  name: str
-  shape: str
-  
+class Msg:
+    type: str
+    name: str
+    shape: str
+
+
 @dataclass
-class Srv(): 
-  request: Msg
-  response: Msg
+class Srv:
+    request: Msg
+    response: Msg
 
 
-def unpack_schema(sig, name): 
-  ros_dtype = None
-  shape = None
-  
-  sig = json.loads(sig)[0]
-  
-  if sig["type"] == "tensor":
-    ros_dtype = TENSOR_TO_ROS[sig["tensor-spec"]["dtype"]]
-    shape = sig["tensor-spec"]["shape"]
-    
-  # TODO: fixed size cases
-  return Msg(ros_dtype + "[]", name, shape) 
+def unpack_schema(sig, name):
+    ros_dtype = None
+    shape = None
+
+    sig = json.loads(sig)[0]
+
+    if sig["type"] == "tensor":
+        ros_dtype = TENSOR_TO_ROS[sig["tensor-spec"]["dtype"]]
+        shape = sig["tensor-spec"]["shape"]
+
+    # TODO: fixed size cases
+    return Msg(ros_dtype + "[]", name, shape)
 
 
 def gen_msg(env, model_name, msgs_dir):
-  pyfunc_model = mlflow.pyfunc.load_model(model_uri=join(__location__, LOCAL_URI))
-  sig = pyfunc_model._model_meta._signature.to_dict()
+    pyfunc_model = mlflow.pyfunc.load_model(model_uri=join(__location__, LOCAL_URI))
+    sig = pyfunc_model._model_meta._signature.to_dict()
 
-  req_msg_name = model_name + "_req"
-  res_msg_name = model_name + "_res"
-  
-  req = unpack_schema(sig["inputs"], req_msg_name)
-  res = unpack_schema(sig["outputs"], res_msg_name)
-  
-  service = Srv(req, res)
-  
-  template = env.get_template("service.srv")
-  
-  with open(join(msgs_dir, "srv", f"{model_name}.srv"), "w+") as f:
-    f.write(template.render(asdict(service)))
+    req_msg_name = model_name + "_req"
+    res_msg_name = model_name + "_res"
 
-  return service
+    req = unpack_schema(sig["inputs"], req_msg_name)
+    res = unpack_schema(sig["outputs"], res_msg_name)
+
+    service = Srv(req, res)
+
+    template = env.get_template("service.srv")
+
+    with open(join(msgs_dir, "srv", f"{model_name}.srv"), "w+") as f:
+        f.write(template.render(asdict(service)))
+
+    return service
 
 
 def gen_exec(env, model_name, msg_pkg, srv, exec_dir):
-  render_data = asdict(srv)
-  render_data["model_name"] = model_name
-  render_data["msg_pkg"] = msg_pkg
-  
-  template = env.get_template("exec")
-  
-  with open(join(exec_dir, "scripts", model_name), "w") as f:
-    f.write(template.render(render_data))
+    render_data = asdict(srv)
+    render_data["model_name"] = model_name
+    render_data["msg_pkg"] = msg_pkg
+
+    template = env.get_template("exec")
+
+    with open(join(exec_dir, "scripts", model_name), "w") as f:
+        f.write(template.render(render_data))
 
 
-def gen_pkg(env, model_name, msg_pkg, msg_dir, exec_dir):  
-  # pairs of (<template name>, <output path>)
-  files = (
-    ("msgs_cmake_template.txt", join(msg_dir, "CMakeLists.txt")),
-    ("msgs_package_template.xml", join(msg_dir, "package.xml")),
-    ("exec_cmake_template.txt", join(exec_dir, "CMakeLists.txt")),
-    ("exec_package_template.xml", join(exec_dir, "package.xml")),
-    ("exec_launch_template.launch", join(exec_dir, "launch", f"{model_name}.launch"))
-  )
-  
-  render_data = {"model_name": model_name, "msg_pkg": msg_pkg}
+def gen_pkg(env, model_name, msg_pkg, msg_dir, exec_dir):
+    # pairs of (<template name>, <output path>)
+    files = (
+        ("msgs_cmake_template.txt", join(msg_dir, "CMakeLists.txt")),
+        ("msgs_package_template.xml", join(msg_dir, "package.xml")),
+        ("exec_cmake_template.txt", join(exec_dir, "CMakeLists.txt")),
+        ("exec_package_template.xml", join(exec_dir, "package.xml")),
+        (
+            "exec_launch_template.launch",
+            join(exec_dir, "launch", f"{model_name}.launch"),
+        ),
+    )
 
-  for template_name, output_path in files:
-    template = env.get_template(template_name)
-    
-    with open(output_path, "w") as f:
-      f.write(template.render(render_data))
+    render_data = {"model_name": model_name, "msg_pkg": msg_pkg}
+
+    for template_name, output_path in files:
+        template = env.get_template(template_name)
+
+        with open(output_path, "w") as f:
+            f.write(template.render(render_data))
 
 
 def main():
-  # load jinja environment and read config
-  env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(searchpath=join(__location__, "templates")),
-    autoescape=jinja2.select_autoescape,
-    undefined=jinja2.StrictUndefined
-  )  
-  cfg = ConfigObj(join(__location__, CONFIG_NAME))
-  remote_uri = cfg['MODEL_URI']
-  model_name = remote_uri.split('/')[-2].replace("-", "_")
-  msg_pkg = model_name + "_msgs"
+    # load jinja environment and read config
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(searchpath=join(__location__, "templates")),
+        autoescape=jinja2.select_autoescape,
+        undefined=jinja2.StrictUndefined,
+    )
+    cfg = ConfigObj(join(__location__, CONFIG_NAME))
+    remote_uri = cfg["MODEL_URI"]
+    model_name = remote_uri.split("/")[-2].replace("-", "_")
+    msg_pkg = model_name + "_msgs"
 
-  exec_dir = join(__location__, "rospkg", model_name)
-  msgs_dir = join(__location__, "rospkg", msg_pkg)
-  
-  # create directories
-  Path(join(exec_dir, "launch")).mkdir(parents=True, exist_ok=True)
-  Path(join(exec_dir, "scripts")).mkdir(parents=True, exist_ok=True)
-  Path(join(msgs_dir, "srv")).mkdir(parents=True, exist_ok=True)
+    exec_dir = join(__location__, "rospkg", model_name)
+    msgs_dir = join(__location__, "rospkg", msg_pkg)
 
-  srv = gen_msg(env, model_name, msgs_dir)
-  gen_exec(env, model_name, msg_pkg, srv, exec_dir)
-  gen_pkg(env, model_name, msg_pkg, msgs_dir, exec_dir)
+    # create directories
+    Path(join(exec_dir, "launch")).mkdir(parents=True, exist_ok=True)
+    Path(join(exec_dir, "scripts")).mkdir(parents=True, exist_ok=True)
+    Path(join(msgs_dir, "srv")).mkdir(parents=True, exist_ok=True)
+
+    srv = gen_msg(env, model_name, msgs_dir)
+    gen_exec(env, model_name, msg_pkg, srv, exec_dir)
+    gen_pkg(env, model_name, msg_pkg, msgs_dir, exec_dir)
 
 
 if __name__ == "__main__":
-  main()
+    main()
