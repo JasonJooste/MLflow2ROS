f55154306ecf4e6e2036ed805ebf821a3965f881
Zhirong Lin <zhirong.lin@data61.csiro.au>
2024-01-04 13:06:20 +1000

    Update readme and format app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 import json
-import os
-from dataclasses import asdict, dataclass
-from os.path import join
-from pathlib import Path
-from typing_extensions import Annotated
-from typing import Optional
-from rich import print
-
 import subprocess
-import typer
+from dataclasses import asdict, dataclass
+from os import getcwd
+from os.path import dirname, join, realpath
+from pathlib import Path
+from typing import Optional
+
 import jinja2
 import mlflow
+import rich
+import typer
+from typing_extensions import Annotated
 
 # Dict to convert tensor data types (i.e. numpy) to ROS data types
 # TODO: Finish filling out with:
@@ -21,9 +21,10 @@ import mlflow
 TENSOR_TO_ROS = {"float64": "float64", "int64": "int64"}
 
 # absolute path to the location of this script
-__location__ = os.path.realpath(join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = realpath(join(getcwd(), dirname(__file__)))
 
 app = typer.Typer()
+
 
 @dataclass
 class Msg:
@@ -66,7 +67,7 @@ def gen_msg(env, model_uri, model_name, msgs_dir):
 
     template = env.get_template("service.srv")
 
-    with open(join(msgs_dir, "srv", f"{model_name}.srv"), "w+") as f:
+    with open(join(msgs_dir, "srv", f"{model_name}.srv"), "w+", encoding="UTF-8") as f:
         f.write(template.render(asdict(service)))
 
     return service
@@ -79,7 +80,7 @@ def gen_exec(env, model_name, msg_pkg, srv, exec_dir):
 
     template = env.get_template("exec")
 
-    with open(join(exec_dir, "scripts", model_name), "w") as f:
+    with open(join(exec_dir, "scripts", model_name), "w", encoding="UTF-8") as f:
         f.write(template.render(render_data))
 
 
@@ -101,7 +102,7 @@ def gen_pkg(env, model_name, msg_pkg, msg_dir, exec_dir):
     for template_name, output_path in files:
         template = env.get_template(template_name)
 
-        with open(output_path, "w") as f:
+        with open(output_path, "w", encoding="UTF-8") as f:
             f.write(template.render(render_data))
 
 
@@ -111,22 +112,40 @@ def parse_model_name(remote_uri):
 
 
 @app.command()
-def generate_dockerfile(model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")],
-                        rospkg_directory: Annotated[Optional[str], typer.Argument(
-                            help="Location of the ROS package that will be run on the Docker image")] = "rospkg", 
-                        output_directory: Annotated[Optional[str], typer.Argument(
-                            help="Folder containing the generated files")] = "dockerfile"):
+def generate_dockerfile(
+    model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")],
+    rospkg_directory: Annotated[
+        Optional[str],
+        typer.Argument(
+            help="Location of the ROS package that will be run on the Docker image"
+        ),
+    ] = "rospkg",
+    output_directory: Annotated[
+        Optional[str], typer.Argument(help="Folder containing the generated files")
+    ] = "dockerfile",
+):
     """
     Creates the Dockerfile used to build the image containing the model and its ROS package.
     """
     # get mlflow to generate their docker image
-    subprocess.run(["mlflow", "models", "generate-dockerfile", "--model-uri", model_uri, 
-                    "--output-directory", output_directory], check=True, text=True)
-    
-    with open(join(output_directory, "Dockerfile"), "r") as reader:
+    subprocess.run(
+        [
+            "mlflow",
+            "models",
+            "generate-dockerfile",
+            "--model-uri",
+            model_uri,
+            "--output-directory",
+            output_directory,
+        ],
+        check=True,
+        text=True,
+    )
+
+    with open(join(output_directory, "Dockerfile"), "r", encoding="UTF-8") as reader:
         dockerfile = reader.readlines()
 
-    with open(join(output_directory, "Dockerfile"), "w") as writer:
+    with open(join(output_directory, "Dockerfile"), "w", encoding="UTF-8") as writer:
         # edit mlflow dockerfile
         for line in dockerfile:
             if "FROM ubuntu" in line:
@@ -135,29 +154,37 @@ def generate_dockerfile(model_uri: Annotated[str, typer.Argument(help="Location 
             elif "ENTRYPOINT" in line:
                 # Remove the entrypoint command in the dockerfile as we will define a new one later
                 continue
-            elif "model_dir" in line: 
+            elif "model_dir" in line:
                 # Edit the COPY command to work with our build context
                 line = f"COPY {output_directory}/model_dir /opt/ml/model\n"
 
             writer.write(line)
-    
+
         # add dockerfile addendum
         env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(searchpath=join(__location__, "templates")),
             autoescape=jinja2.select_autoescape,
             undefined=jinja2.StrictUndefined,
-        )   
+        )
 
-        render_data = {"ubuntu_distro": "focal", "ros_distro": "noetic", "model_name": parse_model_name(model_uri),
-                       "rospkg_dir": rospkg_directory}
-        
+        render_data = {
+            "ubuntu_distro": "focal",
+            "ros_distro": "noetic",
+            "model_name": parse_model_name(model_uri),
+            "rospkg_dir": rospkg_directory,
+        }
+
         template = env.get_template("addendum.Dockerfile")
         writer.write(template.render(render_data))
 
 
 @app.command()
-def generate_rospkg(model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")], 
-                    output_directory: Annotated[Optional[str], typer.Argument(help="Folder containing the generated files")] = "rospkg"):
+def generate_rospkg(
+    model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")],
+    output_directory: Annotated[
+        Optional[str], typer.Argument(help="Folder containing the generated files")
+    ] = "rospkg",
+):
     """
     Creates the source files for the model's ROS node and message files.
     """
@@ -184,8 +211,10 @@ def generate_rospkg(model_uri: Annotated[str, typer.Argument(help="Location of t
 
 
 @app.command()
-def make_image(model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")],
-               image_name: Annotated[Optional[str], typer.Argument()] = None):
+def make_image(
+    model_uri: Annotated[str, typer.Argument(help="Location of the MLFLow model")],
+    image_name: Annotated[Optional[str], typer.Argument()] = None,
+):
     """
     Builds a Docker image containing the generated ROS node.
     """
@@ -193,17 +222,24 @@ def make_image(model_uri: Annotated[str, typer.Argument(help="Location of the ML
     generate_rospkg(model_uri)
 
     # create docker build command
-    cmd = ["docker", "build", "--file", join(__location__, "dockerfile", "Dockerfile"), "--target", "prod"]
+    cmd = [
+        "docker",
+        "build",
+        "--file",
+        join(__location__, "dockerfile", "Dockerfile"),
+        "--target",
+        "prod",
+    ]
 
-    if not (image_name is None):
+    if not image_name is None:
         cmd = cmd + ["--tag", image_name]
 
     cmd.append(__location__)
 
-    print(f"Running command: '{' '.join(cmd)}'")
+    rich.print(f"Running command: '{' '.join(cmd)}'")
 
     subprocess.run(cmd, check=True, text=True)
-    
+
 
 if __name__ == "__main__":
     app()
