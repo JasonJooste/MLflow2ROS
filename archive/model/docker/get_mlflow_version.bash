952aaab6f4be2ae8ada896e109e26c3586c6a520
Zhirong Lin <zhirong.lin@data61.csiro.au>
2024-01-18 16:29:16 +1000

    Remove archive folder

--- archive/model/docker/get_mlflow_version.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Use a mlflow docker image to access the version number of the production version of a registered model
-MODEL="tracking-quickstart"
-FILTER_STR="name=\'$MODEL\'"
-STAGE=Production
-VERSION_CMD="import mlflow;v=[m for m in mlflow.search_model_versions(filter_string=\"$FILTER_STR\") if m.current_stage==\"$STAGE\"][0].version;print(v)"
-MLFLOW_TRACKING_URI="http://ras-b2-ph.nexus.csiro.au:5000"
-MODEL_VERSION=$(docker run -e MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI ghcr.io/mlflow/mlflow /usr/local/bin/python -c "$VERSION_CMD")
-echo $MODEL_VERSION
