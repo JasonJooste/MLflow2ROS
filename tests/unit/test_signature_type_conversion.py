06c1678934cdc0c529bb13bbff43205634832a53
Zhirong Lin <zhirong.lin@data61.csiro.au>
2024-02-13 11:57:11 +1000

    Update unit test to work with new Msg dataclass

--- tests/unit/test_signature_type_conversion.py
+++ tests/unit/test_signature_type_conversion.py
@@ -7,6 +7,6 @@ def test_tensor_basic():
                   '[{"type": "tensor", "tensor-spec": {"dtype": "int64", "shape": [-1]}}]',
                 'params': None}
     model_name = "tensor_basic"
-    expected = app.Srv(request=app.Msg(type="float64[]", name=f"{model_name}_req", shape=[-1, 4]),
-                       response=app.Msg(type="int64[]", name=f"{model_name}_res", shape=[-1]))
+    expected = app.Srv(request=app.Msg(ros_dtype="float64[]", name=f"{model_name}_req", shape=[-1, 4], base_dtype="float64"),
+                       response=app.Msg(ros_dtype="int64[]", name=f"{model_name}_res", shape=[-1], base_dtype="int64"))
     assert app.sig_to_srv(model_name, sig_dict) == expected
